4c8f916eb1a4eaa72092acba2b3e23cd4b42a2c0
Marco Servidio <marcoservidio@Marcos-Mac-mini.local>
2016-09-18 16:11:17 +0100

    checking equality cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,8 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTwo === cardFour) {
+	alert('You found a match!')
+} else {
+	alert('Sorry, try again.')
+}
